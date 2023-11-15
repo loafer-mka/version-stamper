7c039c08268c65c4fa77902336ced15d239cc1b7
Andrey Makarov <mka-at-mailru@mail.ru>
2023-11-16 00:37:33 +0300

    Test and fix under git-for-windows 2.42.0.windows.2

--- version-stamper.bat
+++ version-stamper.bat
@@ -8,6 +8,7 @@ rem +--------------------------------------------------------------------+
 
 if "%1" == "GIT-PATH" goto :GIT-PATH
 if "%1" == "SHELL-PATH" goto :SHELL-PATH
+if "%1" == "CYGPATH-PATH" goto :CYGPATH-PATH
 goto :main
 
 :GIT-PATH
@@ -17,6 +18,11 @@ goto :EOF
 :SHELL-PATH
 echo %~dps2..
 WHERE "%~dps2\usr\bin;%~dps2\..\usr\bin;%~dps2\bin;%~dps2\..\bin;%~dps2;%~dps2\..:sh" "%~dps2\usr\bin;%~dps2\..\usr\bin;%~dps2\bin;%~dps2\..\bin;%~dps2;%~dps2\..:bash" 2>NUL
+goto :EOF
+
+:CYGPATH-PATH
+echo %~dps2..
+WHERE "%~dps2\usr\bin;%~dps2\..\usr\bin;%~dps2\bin;%~dps2\..\bin;%~dps2;%~dps2\..:cygpath" "%~dps2\usr\bin;%~dps2\..\usr\bin;%~dps2\bin;%~dps2\..\bin;%~dps2;%~dps2\..:cygpath" 2>NUL
 goto :EOF
 
 :main
@@ -40,5 +46,18 @@ if "!SHELLUTIL!" == "" (
 	goto :EOF
 )
 
-!SHELLUTIL! -c "%~dpn0" %*
+set PATHUTIL=
+for /F "tokens=*" %%P in ('"%~0" CYGPATH-PATH !GITPATH!') do set PATHUTIL=%%~sP
+if "!PATHUTIL!" == "" (
+	echo FATAL: %0 cannot find bash.exe and/or sh.exe utility, install please git for windows https://git-scm.com/download/win/
+	goto :EOF
+)
+
+set STAMPER='%~dpn0'
+for /F "tokens=*" %%P in ('"!PATHUTIL!" -m !STAMPER!') do (
+	!SHELLUTIL! -c '"%%P" %*'
+	exit /b %errorlevel%
+)
+
+!SHELLUTIL! -c '"%~dpn0" %*'
 exit /b %errorlevel%

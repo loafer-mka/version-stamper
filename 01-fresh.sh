a420acf924fbc17c77b19ceaff292f36ea7c4973
Andrey Makarov <mka-at-mailru@mail.ru>
2023-11-21 22:35:00 +0300

    Add license notes, add copyright and project home reference; Apply master's #9dfff81 commit

--- 01-fresh.sh
+++ 01-fresh.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# +--------------------------------------------------------------------+
+# | This Source Code Form is subject to the terms of the Mozilla       |
+# | Public License, v. 2.0. If a copy of the MPL was not distributed   |
+# | with this file, You can obtain one at http://mozilla.org/MPL/2.0/. |
+# +--------------------------------------------------------------------+
+#        email: Andrey Makarov <mka-at-mailru@mail.ru>
+# Project home: https://github.com/loafer-mka/version-stamper.git
+#
+#               Copyright (c) Andrey Makarov 2023
+
 . "$(dirname "$0")/lib.sh"
 
 pushd "$(dirname "$0")/repos" >/dev/null 2>&1
--- lib.sh
+++ lib.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# +--------------------------------------------------------------------+
+# | This Source Code Form is subject to the terms of the Mozilla       |
+# | Public License, v. 2.0. If a copy of the MPL was not distributed   |
+# | with this file, You can obtain one at http://mozilla.org/MPL/2.0/. |
+# +--------------------------------------------------------------------+
+#        email: Andrey Makarov <mka-at-mailru@mail.ru>
+# Project home: https://github.com/loafer-mka/version-stamper.git
+#
+#               Copyright (c) Andrey Makarov 2023
+
 # disable history support; this enables 'echo "foo!"' (this sample with enabled history produces 'bash: !: event not found')
 set +H
 # disable monitor
